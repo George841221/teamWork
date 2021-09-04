95156caf0ea02ee9c4c7fd073aaafe7e8eb343fd
Bebyno <bebyno888@gmail.com>
2021-09-04 16:07:19 +0200

    Added DBHelper

---
+++ src/main/java/dataBase/DBHelper.java
@@ -0,0 +1,11 @@
+package dataBase;
+
+public class DBHelper {
+    public static final String TABLE_PLAYER = "player";
+    public static final String TABLE_PLAYABLE_CHARACTER = "playable_character";
+    public static final String TABLE_PLAYER_CHARACTER = "player_character";
+    public static final String TABLE_GAME_SESSION = "game_session";
+   // public static final String TABLE_PLAYER = "game_session_and_character";
+
+
+}
